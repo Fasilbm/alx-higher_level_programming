fa5160ccffce00bf8c133c0587239022d8b56731
Gedi Ezra <fastcaserouge19@gmail.com>
2022-06-04 18:30:46 +0300

    gedi

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
-for num in range(0, 100):
-        if num != 99:
-                print("{:02d}, ".format(num), end='')
-        else:
-                print("{:02d}".format(num))
+for i in range(00, 100):
+    if i <= 9:
+        i = str(i)
+        print("0" + i, end=" ,")
+    else:
+        print(i, end=" ,")
